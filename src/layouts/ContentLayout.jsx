043c42442738ec0581aeb9e68350013249e868c2
Brendan Lynch <main@brenml.com>
2026-09-11 09:09:41 -0400

    updated styling and logos

--- src/layouts/ContentLayout.jsx
+++ src/layouts/ContentLayout.jsx
@@ -7,8 +7,8 @@ export default function ContentLayout() {
         <div className="flex flex-col min-h-dvh max-w-7xl mx-auto sm:p-4 px-4 justify-center">
             <header className="pt-6">
                 <NavLink to="/projects" end>
-                    <h1 className="justify-start text-left text-xl tracking-tighter mb-2">
-                        bwoodwork
+                    <h1 className="justify-start text-left text-xl tracking-tighter mb-2 font-accent">
+                        bwoodwork.
                     </h1>
                 </NavLink>
                 <Navigation />
